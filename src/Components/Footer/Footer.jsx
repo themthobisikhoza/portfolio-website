c86ea7f081c08d636828cad2f6a51ef31589d2c5
Mthobisi Khoza <khozamthobisi7@gmail.com>
2025-12-24 21:27:01 +0200

    Tried to fix footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className="glass-footer">
-            <p className="footer-text"> © 2025 Mthobisi Khoza. All rights reserved | Full Stack Developer | Data Analyst </p>
+            <p className="footer-text"> © 2026 Mthobisi Khoza. All rights reserved | Full Stack Developer | Data Analyst </p>
             <p className="footer-text"> +27 69 569 1119 | khozamthobisi7@gmail.com</p>
         </footer>
     )
